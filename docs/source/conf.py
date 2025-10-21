6f978c285fc37cf73bb43ae30c50e8bf1a9bc995
Gui Castelao <guilherme@castelao.net>
2025-10-21 12:56:35 -0600

    cfg: Extending sufix covered

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,7 +78,11 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ".rst"
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # source_parsers = {'.md': MarkdownParser}
 
