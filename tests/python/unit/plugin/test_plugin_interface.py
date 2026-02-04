5de50e9cc4009a7b4442bd19c0d437708cb66b77
ppinchuk <pinchukpaul@gmail.com>
2026-02-03 19:17:28 -0700

    Add test file

---
+++ tests/python/unit/plugin/test_plugin_interface.py
@@ -0,0 +1,176 @@
+"""COMPASS web crawling tests"""
+
+from pathlib import Path
+
+import pytest
+
+from compass.plugin.interface import ExtractionPlugin
+from compass.exceptions import COMPASSPluginConfigurationError
+
+
+def test_plugin_validation_parse_key_same():
+    """Test plugin interface validation logic"""
+
+    class COLL1:
+        OUT_LABEL = "collected"
+
+    class EXT1:
+        IN_LABEL = "collected"
+        OUT_LABEL = "extracted"
+
+    class EXT2:
+        IN_LABEL = "collected"
+        OUT_LABEL = "extracted_2"
+
+    class PARS1:
+        IN_LABEL = "extracted"
+        OUT_LABEL = "parsed_1"
+
+    class PARS2:
+        IN_LABEL = "collected"
+        OUT_LABEL = "parsed_1"
+
+    class MYPlugin(ExtractionPlugin):
+        TEXT_COLLECTORS = [COLL1]
+        TEXT_EXTRACTORS = [EXT1, EXT2]
+        PARSERS = [PARS1, PARS2]
+
+        IDENTIFIER = "test"
+        WEBSITE_KEYWORDS = []
+        QUESTION_TEMPLATES = []
+        heuristic = None
+
+    with pytest.raises(
+        COMPASSPluginConfigurationError,
+        match="Multiple processing classes produce the same OUT_LABEL key",
+    ):
+        MYPlugin(None, None, None)
+
+
+def test_plugin_validation_extract_key_same():
+    """Test plugin interface validation logic"""
+
+    class COLL1:
+        OUT_LABEL = "collected"
+
+    class EXT1:
+        IN_LABEL = "collected"
+        OUT_LABEL = "extracted"
+
+    class EXT2:
+        IN_LABEL = "collected"
+        OUT_LABEL = "extracted"
+
+    class PARS1:
+        IN_LABEL = "extracted"
+        OUT_LABEL = "parsed_1"
+
+    class PARS2:
+        IN_LABEL = "collected"
+        OUT_LABEL = "parsed_2"
+
+    class MYPlugin(ExtractionPlugin):
+        TEXT_COLLECTORS = [COLL1]
+        TEXT_EXTRACTORS = [EXT1, EXT2]
+        PARSERS = [PARS1, PARS2]
+
+        IDENTIFIER = "test"
+        WEBSITE_KEYWORDS = []
+        QUESTION_TEMPLATES = []
+        heuristic = None
+
+    with pytest.raises(
+        COMPASSPluginConfigurationError,
+        match="Multiple processing classes produce the same OUT_LABEL key",
+    ):
+        MYPlugin(None, None, None)
+
+
+def test_plugin_validation_no_in_key_for_extract():
+    """Test plugin interface validation logic"""
+
+    class COLL1:
+        OUT_LABEL = "collected"
+
+    class EXT1:
+        IN_LABEL = "collected"
+        OUT_LABEL = "extracted"
+
+    class EXT2:
+        IN_LABEL = "collected_2"
+        OUT_LABEL = "extracted_1"
+
+    class PARS1:
+        IN_LABEL = "extracted"
+        OUT_LABEL = "parsed_1"
+
+    class PARS2:
+        IN_LABEL = "collected"
+        OUT_LABEL = "parsed_2"
+
+    class MYPlugin(ExtractionPlugin):
+        TEXT_COLLECTORS = [COLL1]
+        TEXT_EXTRACTORS = [EXT1, EXT2]
+        PARSERS = [PARS1, PARS2]
+
+        IDENTIFIER = "test"
+        WEBSITE_KEYWORDS = []
+        QUESTION_TEMPLATES = []
+        heuristic = None
+
+    with pytest.raises(
+        COMPASSPluginConfigurationError,
+        match=(
+            r"One or more processing classes require IN_LABEL 'collected_2', "
+            r"which is not produced by any previous processing class: "
+            r"\['EXT2'\]"
+        ),
+    ):
+        MYPlugin(None, None, None)
+
+
+def test_plugin_validation_no_in_key_for_parse():
+    """Test plugin interface validation logic"""
+
+    class COLL1:
+        OUT_LABEL = "collected"
+
+    class EXT1:
+        IN_LABEL = "collected"
+        OUT_LABEL = "extracted"
+
+    class EXT2:
+        IN_LABEL = "collected"
+        OUT_LABEL = "extracted_1"
+
+    class PARS1:
+        IN_LABEL = "extracted"
+        OUT_LABEL = "parsed_1"
+
+    class PARS2:
+        IN_LABEL = "collected_2"
+        OUT_LABEL = "parsed_2"
+
+    class MYPlugin(ExtractionPlugin):
+        TEXT_COLLECTORS = [COLL1]
+        TEXT_EXTRACTORS = [EXT1, EXT2]
+        PARSERS = [PARS1, PARS2]
+
+        IDENTIFIER = "test"
+        WEBSITE_KEYWORDS = []
+        QUESTION_TEMPLATES = []
+        heuristic = None
+
+    with pytest.raises(
+        COMPASSPluginConfigurationError,
+        match=(
+            r"One or more processing classes require IN_LABEL 'collected_2', "
+            r"which is not produced by any previous processing class: "
+            r"\['PARS2'\]"
+        ),
+    ):
+        MYPlugin(None, None, None)
+
+
+if __name__ == "__main__":
+    pytest.main(["-q", "--show-capture=all", Path(__file__), "-rapP"])
