c8538e2fcdaa0a492bdf6401dec35aec3518b44e
Paul <pinchukpaul@gmail.com>
2026-05-03 14:04:00 -0600

    Move subprocessing logging messages to `main.log`

--- compass/services/cpu.py
+++ compass/services/cpu.py
@@ -2,14 +2,17 @@
 
 import ast
 import os
+import sys
 import time
 import asyncio
+import logging
 import contextlib
 import warnings
 from io import BytesIO
 from pathlib import Path
 from functools import partial
 from concurrent.futures import ProcessPoolExecutor
+from logging.handlers import QueueHandler
 
 import numpy as np
 from elm.web.document import PDFDocument, MDDocument
@@ -28,6 +31,7 @@ from docling.datamodel.pipeline_options import (
 )
 
 from compass.services.base import Service
+from compass.utilities.logs import AddLocationFilter, LQ
 
 
 class ProcessPoolService(Service):
@@ -49,7 +53,16 @@ class ProcessPoolService(Service):
     def acquire_resources(self):
         """Open thread pool and temp directory"""
         os.environ.setdefault("OMP_NUM_THREADS", "1")
-        self.pool = ProcessPoolExecutor(**self._ppe_kwargs)
+        ppe_kwargs = dict(self._ppe_kwargs)
+        user_initializer = ppe_kwargs.pop("initializer", None)
+        initargs = tuple(ppe_kwargs.pop("initargs", ()))
+        ppe_kwargs["initializer"] = _configure_subprocess_logging
+        ppe_kwargs["initargs"] = (
+            LQ.QUEUE,
+            user_initializer,
+            initargs,
+        )
+        self.pool = ProcessPoolExecutor(**ppe_kwargs)
 
     def release_resources(self):
         """Shutdown thread pool and cleanup temp directory"""
@@ -91,121 +104,6 @@ class FileLoader(ProcessPoolService):
 
 class OCRPDFLoader(FileLoader):
     """Loader service for OCR"""
-
-
-def _read_pdf(pdf_bytes, **kwargs):
-    """Utility func so that pdftotext.PDF doesn't have to be pickled"""
-    pages = read_pdf(pdf_bytes, verbose=False)
-    return PDFDocument(pages, **kwargs)
-
-
-def _read_pdf_ocr(pdf_bytes, tesseract_cmd, **kwargs):
-    """Utility function that mimics `_read_pdf`"""
-    if tesseract_cmd:
-        _configure_pytesseract(tesseract_cmd)
-
-    pages = read_pdf_ocr(pdf_bytes, verbose=False)
-    doc = PDFDocument(_try_decode_ocr_pages(pages), **kwargs)
-    doc.attrs["from_ocr"] = True
-    return doc
-
-
-def _read_pdf_file(pdf_fp, **kwargs):
-    """Utility func so that pdftotext.PDF doesn't have to be pickled"""
-    pdf_bytes = Path(pdf_fp).read_bytes()
-    pages = read_pdf(pdf_bytes, verbose=False)
-    return PDFDocument(pages, **kwargs), pdf_bytes
-
-
-def _read_pdf_file_ocr(pdf_fp, tesseract_cmd, **kwargs):
-    """Utility function that mimics `_read_pdf_file`"""
-    if tesseract_cmd:
-        _configure_pytesseract(tesseract_cmd)
-
-    pdf_bytes = Path(pdf_fp).read_bytes()
-    pages = read_pdf_ocr(pdf_bytes, verbose=False)
-    doc = PDFDocument(_try_decode_ocr_pages(pages), **kwargs)
-    doc.attrs["from_ocr"] = True
-    return doc, pdf_bytes
-
-
-def _read_docling(doc_bytes, url, headers=None, **kwargs):
-    """Utility func to read documents using Docling"""
-
-    url = str(url)
-    if headers is not None:
-        headers = dict(headers)
-
-    pipeline_options = PdfPipelineOptions()
-    pipeline_options.do_ocr = True
-    pipeline_options.do_table_structure = True
-    pipeline_options.table_structure_options = TableStructureOptions(
-        do_cell_matching=True
-    )
-    pipeline_options.ocr_options = TesseractCliOcrOptions()
-    html_backend_options = HTMLBackendOptions(source_uri=url)
-
-    doc_converter = DocumentConverter(
-        format_options={
-            InputFormat.PDF: PdfFormatOption(
-                pipeline_options=pipeline_options
-            ),
-            InputFormat.HTML: HTMLFormatOption(
-                backend_options=html_backend_options
-            ),
-        }
-    )
-
-    start_time = time.monotonic()
-    stream = DocumentStream(name=url, stream=BytesIO(doc_bytes))
-    conv_result = doc_converter.convert(stream, headers=headers)
-    conversion_time_seconds = time.monotonic() - start_time
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", RuntimeWarning)
-        mean_confidence = conv_result.confidence.mean_score
-        low_score_confidence = conv_result.confidence.low_score
-
-    attrs = {
-        "doc_filename": conv_result.input.file.stem,
-        "doc_type": conv_result.input.format.value,
-        "conversion_time_seconds": conversion_time_seconds,
-        "from_ocr": any(
-            ~np.isnan(c.ocr_score)
-            for c in conv_result.confidence.pages.values()
-        ),
-        "mean_confidence": mean_confidence,
-        "low_score_confidence": low_score_confidence,
-    }
-    doc_text = conv_result.document.export_to_markdown(**kwargs)
-
-    return MDDocument([doc_text], attrs=attrs, remove_comments=False)
-
-
-def _read_file_docling(fp, **kwargs):
-    """Read a local file using Docling"""
-
-    fp = Path(fp)
-    doc_bytes = fp.read_bytes()
-    doc = _read_docling(doc_bytes, fp, headers=None, **kwargs)
-    return doc, doc_bytes
-
-
-def _configure_pytesseract(tesseract_cmd):
-    """Set the tesseract_cmd"""
-    import pytesseract  # noqa: PLC0415
-
-    pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
-
-
-def _try_decode_ocr_pages(pages):
-    """Try to decode pages into strings"""
-    decoded_pages = []
-    for page in pages:
-        with contextlib.suppress(Exception):
-            page = ast.literal_eval(page).decode("utf-8")  # noqa: PLW2901
-        decoded_pages.append(page)
-    return decoded_pages
 
 
 async def read_pdf_doc(pdf_bytes, **kwargs):
@@ -352,3 +250,186 @@ async def read_docling_local_file(fp, **kwargs):
         Raw bytes of the PDF file.
     """
     return await FileLoader.call(_read_file_docling, fp, **kwargs)
+
+
+def _read_pdf(pdf_bytes, **kwargs):
+    """Utility func so that pdftotext.PDF doesn't have to be pickled"""
+    pages = read_pdf(pdf_bytes, verbose=False)
+    return PDFDocument(pages, **kwargs)
+
+
+def _read_pdf_ocr(pdf_bytes, tesseract_cmd, **kwargs):
+    """Utility function that mimics `_read_pdf`"""
+    if tesseract_cmd:
+        _configure_pytesseract(tesseract_cmd)
+
+    pages = read_pdf_ocr(pdf_bytes, verbose=False)
+    doc = PDFDocument(_try_decode_ocr_pages(pages), **kwargs)
+    doc.attrs["from_ocr"] = True
+    return doc
+
+
+def _read_pdf_file(pdf_fp, **kwargs):
+    """Utility func so that pdftotext.PDF doesn't have to be pickled"""
+    pdf_bytes = Path(pdf_fp).read_bytes()
+    pages = read_pdf(pdf_bytes, verbose=False)
+    return PDFDocument(pages, **kwargs), pdf_bytes
+
+
+def _read_pdf_file_ocr(pdf_fp, tesseract_cmd, **kwargs):
+    """Utility function that mimics `_read_pdf_file`"""
+    if tesseract_cmd:
+        _configure_pytesseract(tesseract_cmd)
+
+    pdf_bytes = Path(pdf_fp).read_bytes()
+    pages = read_pdf_ocr(pdf_bytes, verbose=False)
+    doc = PDFDocument(_try_decode_ocr_pages(pages), **kwargs)
+    doc.attrs["from_ocr"] = True
+    return doc, pdf_bytes
+
+
+def _read_docling(doc_bytes, url, headers=None, **kwargs):
+    """Utility func to read documents using Docling"""
+
+    url = str(url)
+    if headers is not None:
+        headers = dict(headers)
+
+    pipeline_options = PdfPipelineOptions()
+    pipeline_options.do_ocr = True
+    pipeline_options.do_table_structure = True
+    pipeline_options.table_structure_options = TableStructureOptions(
+        do_cell_matching=True
+    )
+    pipeline_options.ocr_options = TesseractCliOcrOptions()
+    html_backend_options = HTMLBackendOptions(source_uri=url)
+
+    doc_converter = DocumentConverter(
+        format_options={
+            InputFormat.PDF: PdfFormatOption(
+                pipeline_options=pipeline_options
+            ),
+            InputFormat.HTML: HTMLFormatOption(
+                backend_options=html_backend_options
+            ),
+        }
+    )
+
+    start_time = time.monotonic()
+    stream = DocumentStream(name=url, stream=BytesIO(doc_bytes))
+    conv_result = doc_converter.convert(stream, headers=headers)
+    conversion_time_seconds = time.monotonic() - start_time
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", RuntimeWarning)
+        mean_confidence = conv_result.confidence.mean_score
+        low_score_confidence = conv_result.confidence.low_score
+
+    attrs = {
+        "doc_filename": conv_result.input.file.stem,
+        "doc_type": conv_result.input.format.value,
+        "conversion_time_seconds": conversion_time_seconds,
+        "from_ocr": any(
+            ~np.isnan(c.ocr_score)
+            for c in conv_result.confidence.pages.values()
+        ),
+        "mean_confidence": mean_confidence,
+        "low_score_confidence": low_score_confidence,
+    }
+    doc_text = conv_result.document.export_to_markdown(**kwargs)
+
+    return MDDocument([doc_text], attrs=attrs, remove_comments=False)
+
+
+def _read_file_docling(fp, **kwargs):
+    """Read a local file using Docling"""
+
+    fp = Path(fp)
+    doc_bytes = fp.read_bytes()
+    doc = _read_docling(doc_bytes, fp, headers=None, **kwargs)
+    return doc, doc_bytes
+
+
+def _configure_pytesseract(tesseract_cmd):
+    """Set the tesseract_cmd"""
+    import pytesseract  # noqa: PLC0415
+
+    pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
+
+
+def _try_decode_ocr_pages(pages):
+    """Try to decode pages into strings"""
+    decoded_pages = []
+    for page in pages:
+        with contextlib.suppress(Exception):
+            page = ast.literal_eval(page).decode("utf-8")  # noqa: PLW2901
+        decoded_pages.append(page)
+    return decoded_pages
+
+
+def _configure_subprocess_logging(logging_queue, user_initializer, initargs):
+    """Route subprocess output through the main process log queue"""
+    queue_handler = QueueHandler(logging_queue)
+    queue_handler.addFilter(AddLocationFilter())
+
+    root_logger = logging.getLogger()
+    root_logger.handlers = []
+    root_logger.addHandler(queue_handler)  # root emits to queue handler
+    root_logger.setLevel(logging.INFO)
+
+    for lib in ("compass", "elm", "docling", "openai"):
+        lib_logger = logging.getLogger(lib)
+        lib_logger.handlers = []  # no handlers within subprocess
+        lib_logger.propagate = True  # instead, propogate to root logger
+        lib_logger.setLevel(logging.INFO)
+
+    stdout_logger = logging.getLogger("compass.subprocess.stdout")
+    stderr_logger = logging.getLogger("compass.subprocess.stderr")
+    stdout_logger.setLevel(logging.INFO)
+    stderr_logger.setLevel(logging.WARNING)
+    sys.stdout = _LogStream(stdout_logger, logging.INFO)
+    sys.stderr = _LogStream(stderr_logger, logging.WARNING)
+
+    if user_initializer is not None:
+        user_initializer(*initargs)
+
+
+class _LogStream:
+    """File-like object that forwards writes into a logger"""
+
+    def __init__(self, logger, level):
+        """
+
+        Parameters
+        ----------
+        logger : logging.Logger
+            Logger to emit redirected stream output to.
+        level : int
+            Logging level used for forwarded messages.
+        """
+        self.logger = logger
+        self.level = level
+        self._buffer = ""
+        self.encoding = "utf-8"
+
+    def write(self, message):
+        """Forward complete lines to the configured logger"""
+        if not message:
+            return 0
+
+        self._buffer += message
+        while "\n" in self._buffer:
+            line, self._buffer = self._buffer.split("\n", 1)
+            if line:
+                self.logger.log(self.level, line)
+        return len(message)
+
+    def flush(self):
+        """Flush any partial line buffered from the stream"""
+        if self._buffer:
+            self.logger.log(self.level, self._buffer)
+            self._buffer = ""
+
+    def isatty(self):  # noqa: PLR6301
+        """bool: Redirected subprocess streams are never TTYs"""
+        return False
