2e8ffe8143ebf442e9bb7a6728a2dd36574ee7aa
Paul <pinchukpaul@gmail.com>
2026-05-26 20:17:48 -0600

    Add pipeline init

---
+++ compass/pipeline/__init__.py
@@ -0,0 +1,14 @@
+"""COMPASS processing pipeline"""
+
+from compass.pipeline.data_classes import (
+    BaseRequest,
+    CollectionRequest,
+    ExtractionRequest,
+    KnownSourcesInput,
+    ModelSelection,
+    OutputSettings,
+    ProcessRequest,
+    RuntimeSettings,
+    SearchSettings,
+    JurisdictionResult,
+)
