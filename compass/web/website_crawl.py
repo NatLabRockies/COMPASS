66cdaff194287b5376ec2e5fa33afdf332a7820a
Paul <pinchukpaul@gmail.com>
2026-07-20 18:34:25 -0600

    Fix error message

--- compass/web/website_crawl.py
+++ compass/web/website_crawl.py
@@ -475,7 +475,7 @@ class COMPASSCrawler:
     def _log_crawl_stats(self):
         """Log statistics about crawled pages and depths"""
         logger.info("Crawled %d pages", len(self._already_visited))
-        logger.info("Found %d potential documents", len(self._out_docs))
+        logger.info("Found %d potential document(s)", len(self._out_docs))
         logger.debug("Average score: %.2f", self._compute_avg_link_score())
 
         logger.debug("Pages crawled by depth:")
