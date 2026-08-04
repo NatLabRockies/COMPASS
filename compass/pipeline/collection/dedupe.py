d43b1d746bbb24d4d57874c5045a4c5b4e375afe
Paul <pinchukpaul@gmail.com>
2026-08-04 11:30:17 -0600

    New func input

--- compass/pipeline/collection/dedupe.py
+++ compass/pipeline/collection/dedupe.py
@@ -34,7 +34,7 @@ class DocumentDeDuplicator:
         logger.debug("Adding %d doc(s) to collection", len(docs))
         for doc in docs:
             doc.attrs.setdefault("jurisdiction_name", jurisdiction_name)
-            key = _collection_doc_key(doc)
+            key = _collection_doc_key(doc.attrs)
             entry = self._docs.setdefault(key, {"doc": doc, "from_steps": []})
             if step_name not in entry["from_steps"]:
                 entry["from_steps"].append(step_name)
