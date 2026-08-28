56f914a05588130d03c8255a55db764b5f7e2fb2
Paul <pinchukpaul@gmail.com>
2026-08-28 17:38:38 -0600

    MInor refactor

--- compass/pipeline/collection/base.py
+++ compass/pipeline/collection/base.py
@@ -133,11 +133,7 @@ class DocumentCollection:
                     )
                 )
                 if context is not None:
-                    for doc in context.data_docs:
-                        doc.attrs["from_steps"] = list(
-                            self.de_duplicator.info(doc).from_steps
-                        )
-                    return context
+                    return self._context_with_documented_steps(context)
             else:
                 self._collection_info = (
                     await self.workflow.write_collection_shard_no_fail(
@@ -185,6 +181,14 @@ class DocumentCollection:
         self.de_duplicator.add_docs(docs, step_name=str(step.STEP_NAME))
         self._completed_steps.add(step.STEP_NAME)
         return docs
+
+    def _context_with_documented_steps(self, context):
+        """Attach collection steps to each document in the context"""
+        for doc in context.data_docs:
+            doc.attrs["from_steps"] = list(
+                self.de_duplicator.info(doc).from_steps
+            )
+        return context
 
     def _log_execute_results(self):
         """Log the results of the collection execution"""
