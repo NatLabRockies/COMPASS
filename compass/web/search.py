1436452b66fedc47c9aab82fae4db32b5e11fc2f
Paul <pinchukpaul@gmail.com>
2026-08-28 17:48:55 -0600

    PR review updates

--- compass/web/search.py
+++ compass/web/search.py
@@ -1,11 +1,13 @@
 """COMPASS ordinance document web search functionality"""
 
 import logging
+from urllib.parse import urlsplit, urlunsplit
 
 from elm.web.search.run import search_with_fallback, search_all_se
 
 
 logger = logging.getLogger(__name__)
+_JURISDICTION_WEBSITE_PLACEHOLDER = "{jurisdiction_website}"
 
 
 async def search_single_jurisdiction(
@@ -74,14 +76,7 @@ async def search_single_jurisdiction(
 
     """
 
-    queries = [
-        query.format(
-            jurisdiction=jurisdiction.full_name,
-            jurisdiction_website=jurisdiction.website_url,
-        )
-        for query in query_templates
-        if "{jurisdiction_website}" not in query or jurisdiction.website_url
-    ]
+    queries = _format_queries(jurisdiction, query_templates)
     base = {
         "jurisdiction": jurisdiction.full_name,
         "state": jurisdiction.state,
@@ -91,6 +86,9 @@ async def search_single_jurisdiction(
         "results": [],
         "error": None,
     }
+    if not queries:
+        return base
+
     run_meth = _run_simple_sort_search if simple else _run_holistic_sort_search
 
     try:
@@ -111,6 +109,33 @@ async def search_single_jurisdiction(
 
     base["results"] = out
     return base
+
+
+def _format_queries(jurisdiction, query_templates):
+    """Format query templates for a given jurisdiction"""
+    jurisdiction_website = _format_jurisdiction_website(jurisdiction)
+    return [
+        query.format(
+            jurisdiction=jurisdiction.full_name,
+            jurisdiction_website=jurisdiction_website,
+        )
+        for query in query_templates
+        if _JURISDICTION_WEBSITE_PLACEHOLDER not in query
+        or jurisdiction.website_url
+    ]
+
+
+def _format_jurisdiction_website(jurisdiction):
+    """Strip paths and ensure a valid URL scheme for a jur website"""
+    jurisdiction_website = jurisdiction.website_url
+    if jurisdiction_website:
+        website_parts = urlsplit(jurisdiction_website)
+        if not website_parts.netloc:
+            website_parts = urlsplit(f"//{jurisdiction_website}")
+        jurisdiction_website = urlunsplit(
+            (website_parts.scheme, website_parts.netloc, "", "", "")
+        ).removeprefix("//")
+    return jurisdiction_website
 
 
 async def _run_simple_sort_search(
